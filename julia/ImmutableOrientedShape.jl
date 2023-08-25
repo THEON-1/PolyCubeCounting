c0738a0d7c61a16454399c3ba583b1888487292b
Maxime Vorwerk <maxime@4-werk.com>
2023-08-25 16:02:16 +0200

    added ImmutableOrientedShapes

---
+++ julia/ImmutableOrientedShape.jl
@@ -0,0 +1,9 @@
+include("Shape.jl")
+
+struct ImmutableOrientedShape
+    cubes::List{Tuple{Int8, Int8, Int8}}
+    hash::UInt
+end
+
+Base.hash(S::ImmutableOrientedShape) = S.hash
+Base.hash(S::ImmutableOrientedShape, h::UInt) = S.hash ⊻ h
