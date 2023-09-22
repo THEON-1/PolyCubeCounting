9ef4308601d677ee7bd1fbb3b41603393309b4ea
Maxime Vorwerk <maxime@4-werk.com>
2023-09-22 11:45:49 +0200

    Changed rotations def

--- julia/Rotations.jl
+++ julia/Rotations.jl
@@ -1,37 +1,36 @@
+_rot01(x::Tuple{Int64, Int64, Int64}) = ( x[1],  x[2],  x[3])
+_rot02(x::Tuple{Int64, Int64, Int64}) = ( x[2],  x[3],  x[1])
+_rot03(x::Tuple{Int64, Int64, Int64}) = ( x[3],  x[1],  x[2])
 
-const global _rot01(x)::Tuple{Int64, Int64, Int64} = ( x[1],  x[2],  x[3])
-const global _rot02(x)::Tuple{Int64, Int64, Int64} = ( x[2],  x[3],  x[1])
-const global _rot03(x)::Tuple{Int64, Int64, Int64} = ( x[3],  x[1],  x[2])
+_rot04(x::Tuple{Int64, Int64, Int64}) = ( x[1], -x[2], -x[3])
+_rot05(x::Tuple{Int64, Int64, Int64}) = ( x[2], -x[3], -x[1])
+_rot06(x::Tuple{Int64, Int64, Int64}) = ( x[3], -x[1], -x[2])
 
-const global _rot04(x)::Tuple{Int64, Int64, Int64} = ( x[1], -x[2], -x[3])
-const global _rot05(x)::Tuple{Int64, Int64, Int64} = ( x[2], -x[3], -x[1])
-const global _rot06(x)::Tuple{Int64, Int64, Int64} = ( x[3], -x[1], -x[2])
+_rot07(x::Tuple{Int64, Int64, Int64}) = (-x[1],  x[2], -x[3])
+_rot08(x::Tuple{Int64, Int64, Int64}) = (-x[2],  x[3], -x[1])
+_rot09(x::Tuple{Int64, Int64, Int64}) = (-x[3],  x[1], -x[2])
 
-const global _rot07(x)::Tuple{Int64, Int64, Int64} = (-x[1],  x[2], -x[3])
-const global _rot08(x)::Tuple{Int64, Int64, Int64} = (-x[2],  x[3], -x[1])
-const global _rot09(x)::Tuple{Int64, Int64, Int64} = (-x[3],  x[1], -x[2])
+_rot10(x::Tuple{Int64, Int64, Int64}) = (-x[1], -x[2],  x[3])
+_rot11(x::Tuple{Int64, Int64, Int64}) = (-x[2], -x[3],  x[1])
+_rot12(x::Tuple{Int64, Int64, Int64}) = (-x[3], -x[1],  x[2])
 
-const global _rot10(x)::Tuple{Int64, Int64, Int64} = (-x[1], -x[2],  x[3])
-const global _rot11(x)::Tuple{Int64, Int64, Int64} = (-x[2], -x[3],  x[1])
-const global _rot12(x)::Tuple{Int64, Int64, Int64} = (-x[3], -x[1],  x[2])
+_rot13(x::Tuple{Int64, Int64, Int64}) = (-x[3],  x[2],  x[1])
+_rot14(x::Tuple{Int64, Int64, Int64}) = (-x[2],  x[1],  x[3])
+_rot15(x::Tuple{Int64, Int64, Int64}) = (-x[1],  x[3],  x[2])
 
-const global _rot13(x)::Tuple{Int64, Int64, Int64} = (-x[3],  x[2],  x[1])
-const global _rot14(x)::Tuple{Int64, Int64, Int64} = (-x[2],  x[1],  x[3])
-const global _rot15(x)::Tuple{Int64, Int64, Int64} = (-x[1],  x[3],  x[2])
+_rot16(x::Tuple{Int64, Int64, Int64}) = ( x[3], -x[2],  x[1])
+_rot17(x::Tuple{Int64, Int64, Int64}) = ( x[2], -x[1],  x[3])
+_rot18(x::Tuple{Int64, Int64, Int64}) = ( x[1], -x[3],  x[2])
 
-const global _rot16(x)::Tuple{Int64, Int64, Int64} = ( x[3], -x[2],  x[1])
-const global _rot17(x)::Tuple{Int64, Int64, Int64} = ( x[2], -x[1],  x[3])
-const global _rot18(x)::Tuple{Int64, Int64, Int64} = ( x[1], -x[3],  x[2])
+_rot19(x::Tuple{Int64, Int64, Int64}) = ( x[3],  x[2], -x[1])
+_rot20(x::Tuple{Int64, Int64, Int64}) = ( x[2],  x[1], -x[3])
+_rot21(x::Tuple{Int64, Int64, Int64}) = ( x[1],  x[3], -x[2])
 
-const global _rot19(x)::Tuple{Int64, Int64, Int64} = ( x[3],  x[2], -x[1])
-const global _rot20(x)::Tuple{Int64, Int64, Int64} = ( x[2],  x[1], -x[3])
-const global _rot21(x)::Tuple{Int64, Int64, Int64} = ( x[1],  x[3], -x[2])
+_rot22(x::Tuple{Int64, Int64, Int64}) = (-x[3], -x[2], -x[1])
+_rot23(x::Tuple{Int64, Int64, Int64}) = (-x[2], -x[1], -x[3])
+_rot24(x::Tuple{Int64, Int64, Int64}) = (-x[1], -x[3], -x[2])
 
-const global _rot22(x)::Tuple{Int64, Int64, Int64} = (-x[3], -x[2], -x[1])
-const global _rot23(x)::Tuple{Int64, Int64, Int64} = (-x[2], -x[1], -x[3])
-const global _rot24(x)::Tuple{Int64, Int64, Int64} = (-x[1], -x[3], -x[2])
-
-const global Rotations = [
+const global Rotations = (;
     _rot01,
     _rot02,
     _rot03,
@@ -56,4 +55,4 @@ const global Rotations = [
     _rot22,
     _rot23,
     _rot24
-]
+)
