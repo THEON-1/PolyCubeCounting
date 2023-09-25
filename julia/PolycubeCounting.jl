1f84c982f665f25dcb978de6422ca6626c77084f
Maxime Vorwerk <maxime@4-werk.com>
2023-09-25 03:59:32 +0200

    removed command line interface

--- julia/PolycubeCounting.jl
+++ julia/PolycubeCounting.jl
@@ -4,46 +4,7 @@ include("TupleMisc.jl")
 include("plot.jl")
 using XXhash
 using Serialization
-using ArgParse
 using Combinatorics
-
-function main()
-    s = ArgParseSettings()
-    @add_arg_table s begin
-        "-g"
-            help = "generator"
-            arg_type = Int
-        "-c"
-            help = "Polycube count"
-            action = :store_true
-        "-l"
-            help = "Polycube list"
-            action = :store_true
-        "-p"
-            help = "plot n_cubes i_Polycube"
-            nargs = '+'
-            arg_type = Int
-    end
-
-    parsed_args = parse_args(s)
-
-    generate = get(parsed_args, "g", nothing)
-    if generate !== nothing && generate > 0
-        scanForPolycubes(generate)
-    end
-    if get(parsed_args, "c", false)
-        countPolycubes()
-    end
-    if get(parsed_args, "l", false)
-        listPolycubes()
-    end
-    plot = get(parsed_args, "p", nothing)
-    if length(plot) == 1
-        plotPolycubes(plot)
-    elseif length(plot) == 2
-        plotPolycube(plot)
-    end
-end
 
 function options()
     println("scanForPolycubes(Int::n): scans for Polycubes of size <=n")
