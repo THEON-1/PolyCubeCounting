aa246536573abd3c79f879d3c5ce21acc6f91d62
= <=>
2024-02-06 17:32:04 +0100

    now also encodesd sets

--- python/polycube_encoder.py
+++ python/polycube_encoder.py
@@ -6,4 +6,6 @@ class PolyCubeEncdoder(JSONEncoder):
     def default(self, o: Any) -> Any:
         if isinstance(o, PolyCube):
             return list(o.cubes)
+        elif isinstance(o, set):
+            return list(o)
         return super().default(o)
