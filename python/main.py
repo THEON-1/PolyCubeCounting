7f6ac272c3f5a3f69e9dac0910e30a00d3d1a929
= <=>
2024-02-06 17:35:40 +0100

    main should be done for real testing for now

--- python/main.py
+++ python/main.py
@@ -1,9 +1,12 @@
 from polycube import PolyCube
+from polycube_encoder import PolyCubeEncdoder
 import json
 
 def main(maxlength:int):
-    polycubes = {}
+    polycubes = set([PolyCube([(0, 0, 0)], [(0, 0, 0)])])
+    n_size = [0]*maxlength
     todo = [PolyCube([(0, 0, 0)], [(0, 0, 0)])]
+    n_size[0] = 1
     while len(todo) != 0:
         polycube = todo.pop()
         children = polycube.generate_children(maxlength)
@@ -11,9 +14,12 @@ def main(maxlength:int):
             if child in polycubes:
                 continue
             todo.append(child)
-            polycubes[child] = 0
+            polycubes.add(child)
+            n_size[len(child.cubes)-1] += 1
     with open("out.json", "w") as f:
-        f.write(json.dumps(polycubes))
+        f.write(json.dumps(polycubes, cls=PolyCubeEncdoder))
+    with open("count.json", "w") as f:
+        f.write(json.dumps(n_size))
 
 if __name__ == "__main__":
     main(4)
